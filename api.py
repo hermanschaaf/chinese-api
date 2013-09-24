3eeca4199b5d297ffffc440c699b9925a602aafa
Herman Schaaf <hermanschaaf@gmail.com>
2013-09-24 22:31:15 +0900

    Add allow any origin

--- api.py
+++ api.py
@@ -1,10 +1,51 @@
 import os
 from functools import wraps
-from flask import Flask, request, jsonify, current_app
+from flask import Flask, request, render_template, jsonify, current_app
 
 import mafan
 
 app = Flask(__name__)
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 def jsonp(func):
     """Wraps JSONified output for JSONP requests."""
@@ -15,6 +56,7 @@ def jsonp(func):
             data = str(func(*args, **kwargs).data)
             content = str(callback) + '(' + data + ')'
             mimetype = 'application/javascript'
+            header("Access-Control-Allow-Origin: *");
             return current_app.response_class(content, mimetype=mimetype)
         else:
             return func(*args, **kwargs)
@@ -22,6 +64,7 @@ def jsonp(func):
 
 @app.route('/simplify', methods=['POST', 'GET'])
 @jsonp
+@crossdomain
 def simplify():
     text = request.args.get('text')
     d = {'text': mafan.simplify(text)}
@@ -29,10 +72,15 @@ def simplify():
 
 @app.route('/tradify', methods=['POST', 'GET'])
 @jsonp
+@crossdomain
 def tradify():
     text = request.args.get('text')
     d = {'text': mafan.tradify(text)}
     return jsonify(**d)
+
+@app.route('/')
+def test_bookmarklet():
+    return render_template('chinese-bookmarklet.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
