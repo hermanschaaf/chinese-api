6b6babd28e3d51b951ef88c1f115c544a48c3b5f
Herman Schaaf <hermanschaaf@gmail.com>
2013-10-31 00:05:27 +0900

    Fix dict2js

--- dict2js.py
+++ dict2js.py
@@ -6,17 +6,13 @@ f = codecs.open("data/dict.txt.big", "r", "utf-8")
 w = codecs.open('data/dictionary.js', 'w', 'utf8')
 
 print "writing dictionary..."
-w.write("var dict = [\n");
+w.write("var dictionary = [\n");
 
 for i, line in enumerate(f.readlines()):
 	l = line.split(' ')
-	w.write('[\"%s\",%d,\"%s\"],' % (l[0], int(l[1]), l[2].strip()))
+	w.write("""[\"%s\",%d,\"%s\"],""" % (l[0], int(l[1]), l[2].strip()))
 
-w.write("""];
-
-define(function (require) {
-    return dict;
-});""")
+w.write("""];""")
 
 f.close()
 w.close()
